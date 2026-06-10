213e11cf6d42fc331fc4bab848aba98f74cc419e
andy <hei.andy.100@gmail.com>
2026-06-10 22:23:58 +0300

    feat: restructure book copies and add isbn search endpoint

--- src/main/java/com/example/demo/repository/BookCopyRepository.java
+++ src/main/java/com/example/demo/repository/BookCopyRepository.java
@@ -1,8 +1,11 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.BookCopy;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookCopyRepository extends JpaRepository<BookCopy, String> {}
+public interface BookCopyRepository extends JpaRepository<BookCopy, String> {
+  Optional<BookCopy> findByIsbn(String isbn);
+}
