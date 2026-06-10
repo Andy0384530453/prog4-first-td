032659de999b036ce58e145fee3b61927c7c8293
andy <hei.andy.100@gmail.com>
2026-06-10 22:31:56 +0300

    feat : findBookById not findBookByIsbn

--- src/main/java/com/example/demo/repository/BookCopyRepository.java
+++ src/main/java/com/example/demo/repository/BookCopyRepository.java
@@ -1,11 +1,9 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.BookCopy;
-import java.util.Optional;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookCopyRepository extends JpaRepository<BookCopy, String> {
-  Optional<BookCopy> findByIsbn(String isbn);
-}
+public interface BookCopyRepository extends JpaRepository<BookCopy, UUID> {}
