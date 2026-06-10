174634784de715e72b5b3e0d8f4dca26e49f4936
Miantsa <hei.miantsa.3@gmail.com>
2026-06-10 10:29:15 +0300

    feat : book copy entity and repository

---
+++ src/main/java/com/example/demo/entity/BookCopy.java
@@ -0,0 +1,35 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "book_copy")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BookCopy {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(name = "id_book_copy", updatable = false, nullable = false)
+  private UUID id;
+
+  @ManyToOne
+  @JoinColumn(name = "id_book", nullable = false)
+  private Book book;
+}
--- src/main/java/com/example/demo/repository/BookCopyRepository.java
+++ src/main/java/com/example/demo/repository/BookCopyRepository.java
@@ -1,9 +1,10 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.BookCopy;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface BookCopyRepository extends JpaRepository<> {
+public interface BookCopyRepository extends JpaRepository<BookCopy, String> {
 }
