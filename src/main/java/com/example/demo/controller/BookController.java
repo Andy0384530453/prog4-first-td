1659b414aeeb652cc67106d559d30480d14e72ec
andy <hei.andy.100@gmail.com>
2026-06-18 16:30:41 +0300

    feat : implement get book by id

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -1,12 +1,15 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.BookResponseDto;
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.service.BookService;
 import java.util.List;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +25,18 @@ public class BookController {
     try {
       List<BookResponseDto> books = bookService.getAllBooks();
       return ResponseEntity.ok(books);
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("server error");
+    }
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<?> getBookById(@PathVariable UUID id) {
+    try {
+      BookResponseDto book = bookService.getBookById(id);
+      return ResponseEntity.ok(book);
+    } catch (ResourceNotFoundException e) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Livre introuvable");
     } catch (Exception e) {
       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("server error");
     }
